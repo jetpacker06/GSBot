2ea0d520366f56a112584ff693295da040f55d16
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2025-11-29 15:35:08 -0600

    update realm code

--- src/main/java/com/jetpacker06/gsbot/command/slash/commands/HowToJoinMinecraftCommand.java
+++ src/main/java/com/jetpacker06/gsbot/command/slash/commands/HowToJoinMinecraftCommand.java
@@ -20,7 +20,7 @@ public class HowToJoinMinecraftCommand extends Command {
         event.replyEmbeds(Util.createInstructionalEmbed(
                 "How to join the Minecraft realm",
                 "Make sure you're signed into Minecraft with an Xbox Live account/Microsoft account",
-                "Click the invite link https://realms.gg/tA3hTrhNM8WCSvE or enter the realm code manually: tA3hTrhNM8WCSvE"
+                "Click the invite link https://realms.gg/DZuSyX8ynFgZDtA or enter the realm code manually: DZuSyX8ynFgZDtA"
         )).queue();
     }
 
